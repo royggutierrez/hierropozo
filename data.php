b2f8b9750fee10527334a9bb415e9f3bd0a183b8
Roy Gutierrez <royggutierrez@gmail.com>
2020-09-29 09:40:30 -0500

    Correccion de errorez n1

--- data.php
+++ data.php
@@ -8,9 +8,11 @@
   <?php
     echo "Tu correo es : ";
     echo $_REQUEST['email'];
+    <br>
     echo "Tu nombre de usuario es : ";
     echo $_REQUEST['username'];
     echo "Tu contraseña es : ";
+    <br>
     echo $_REQUEST['password'];
 ?>
 </body>
